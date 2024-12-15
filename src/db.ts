2fbff68be71f6ec16a93b18dee68d01f24f65ebb
anish2210 <anishjaiswal1220@gmail.com>
2024-12-15 20:53:00 +0530

    Added user Schema

---
+++ src/db.ts
@@ -0,0 +1,23 @@
+import mongoose, { model, Schema } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbUri = process.env.DB_URI;
+if (!dbUri) {
+  throw new Error("Database URI not valid");
+}
+mongoose.connect(dbUri).then(() => console.log("Connection to DB successful")).catch((error)=>console.error(error));
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+export const userModel = model("Users", userSchema);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
+import { userModel } from "./db";
 
 const app = express();
+app.use(express.json());
 
-app.post("/api/v1/signin", (req, res)=>{
-
+app.post("/api/v1/signin", async(req, res)=>{
+    const {userName, password} = req.body;
+    await userModel.create({userName, password});
+    res.json({
+        msg: "User Created Successfully"
+    })
 });
 
 app.post("/api/v1/signup", (req, res)=>{
