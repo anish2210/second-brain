bc1b69ff3b6be87559ba79f65045e5a3d5334484
anish2210 <anishjaiswal1220@gmail.com>
2025-01-24 15:46:09 +0530

    minor changes

--- sb-client/src/icons/CrossIcon.tsx
+++ sb-client/src/icons/CrossIcon.tsx
@@ -14,13 +14,13 @@ export function CrossIcon(props: CrossProps) {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="white"
       className={sizeClass[props.size]}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M12 4.5v15m7.5-7.5h-15"
       />
     </svg>
